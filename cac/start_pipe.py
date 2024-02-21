720de45cef0525a4ab6b41566a285a9e373d683f
Haytham Taymour <57227390+htaymour@users.noreply.github.com>
2024-02-21 23:35:02 +0200

    ok

--- cac/start_pipe.py
+++ cac/start_pipe.py
@@ -1,2 +1,5 @@
 # start of the CD pipeline which will choose which script to apply in order to complete the deployment of the pipeline.
+import sys
+
 print ("HELLO WORLD FROM DOCKER CONTANER DEPLOPY SERVER !")
+if len(sys.argv) > 1 : print (sys.argv[-1]) 
